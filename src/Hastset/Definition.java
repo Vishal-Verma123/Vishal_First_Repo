922f45af720941b250d31fa10c81f7072307ee46
sainiprathmesh <sainiprathmesh@gmail.com>
2020-01-16 19:54:08 +0530

    added getter and setter for rno and name

--- src/Hastset/Definition.java
+++ src/Hastset/Definition.java
@@ -1,4 +1,22 @@
 package Hastset;
 
 public class Definition {
+    private int rno;
+    private String name;
+
+    public int getRno() {
+        return rno;
+    }
+
+    public void setRno(int rno) {
+        this.rno = rno;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
